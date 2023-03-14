102d10cb4a01edb22c91a0f25986249e3dcba594
rjoeloemsingh87 <rjoeloemsingh87@gmail.com>
2023-03-14 17:38:50 -0400

    Update Math_Functions.py
    Median

--- Math_Functions.py
+++ Math_Functions.py
@@ -14,4 +14,15 @@ def mean(numbers):
     mean = sum(numbers) / len(numbers)
     return mean
 
+#Median - median(numbers) - numbers = list of values
+def median(numbers):
+    n = len(numbers)
+    sorted_numbers = sorted(numbers)
+    if n % 2 == 0:
+        median1 = sorted_numbers[n//2]
+        median2 = sorted_numbers[n//2 - 1]
+        median = (median1 + median2) / 2
+    else:
+        median = sorted_numbers[n//2]
+    return median
 
