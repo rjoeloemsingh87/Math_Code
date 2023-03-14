b5ed0d6ab60f4a83fd3553cf6ac6171bffc70694
rjoeloemsingh87 <rjoeloemsingh87@gmail.com>
2023-03-14 17:37:56 -0400

    Update Math_Functions.py
    Mean

--- Math_Functions.py
+++ Math_Functions.py
@@ -1,4 +1,4 @@
-#Standard Deviation
+#Standard Deviation - standard_deviatiom(numbers) - numbers = list of values
 import math
 
 def standard_deviation(numbers):
@@ -8,3 +8,10 @@ def standard_deviation(numbers):
     variance = sum(deviations) / (n - 1)
     std_dev = math.sqrt(variance)
     return std_dev
+
+#Mean - mean(numbers) - numbers = list of values
+def mean(numbers):
+    mean = sum(numbers) / len(numbers)
+    return mean
+
+
